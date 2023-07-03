d67835aa1c1c2ea38a407f3454b373f55f5df173
Pierre-Olivier Brillant <PierreOlivierBrillant@users.noreply.github.com>
2023-07-03 13:54:45 -0400

    Add column size option

--- web/src/components/Column/index.tsx
+++ web/src/components/Column/index.tsx
@@ -1,14 +1,17 @@
 import React, { ReactNode } from "react";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
+interface Props {
+  size?: number;
+}
+
 export default function Column({
   children,
+  size,
 }: {
   children: ReactNode;
+  size?: number;
 }): JSX.Element {
-  return (
-    <div className="col">
-      {children}
-    </div>
-  );
+  const strSize = size != null ? "col-" + size : "col";
+  return <div className={strSize}>{children}</div>;
 }
