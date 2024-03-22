279c74120f83ddb163b5c23b492987d984cde091
deguet <joris.deguet@cegepmontpetit.ca>
2024-03-22 08:23:22 -0400

    Refactor with data security functions apart

---
+++ stock/consoleApp/DataSec.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleApp
+{
+    class DataSec
+    {
+        public static string Encrypt(string input)
+        {
+            string result = "";
+            foreach (char c in input)
+            {
+                //Console.WriteLine(c);
+                // get the character with code that doubles ascii code of current char
+                char t = (char)(c * 2);
+                //Console.WriteLine(t);
+                result += t;
+            }
+            return result;
+        }
+
+        public static string Decrypt(string input)
+        {
+            string result = "";
+            foreach (char c in input)
+            {
+                //Console.WriteLine(c);
+                // get the character with code that doubles ascii code of current char
+                char t = (char)(c / 2);
+                //Console.WriteLine(t);
+                result += t;
+            }
+            return result;
+        }
+
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashBytes); // .NET 5 +
+            }
+        }
+
+        //SHA3_256 
+
+        public static string CreateSHA256(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.SHA256 md5 = System.Security.Cryptography.SHA256.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashBytes); // .NET 5 +
+            }
+        }
+
+
+    }
+}
