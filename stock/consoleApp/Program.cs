2b38c4f18eb149d17fcea326e3d13dae72f202ca
deguet <joris.deguet@cegepmontpetit.ca>
2024-03-22 07:42:24 -0400

    Premier jet de l'application c#

---
+++ stock/consoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace consoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(CreateMD5("Hello, World!"));
+        Console.WriteLine(CreateSHA256("Hello, World!"));
+    }
+
+
+    public static string CreateMD5(string input)
+    {
+        // Use input string to calculate MD5 hash
+        using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+        {
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            return Convert.ToHexString(hashBytes); // .NET 5 +
+        }
+    }
+
+    //SHA3_256 
+
+    public static string CreateSHA256(string input)
+    {
+        // Use input string to calculate MD5 hash
+        using (System.Security.Cryptography.SHA256 md5 = System.Security.Cryptography.SHA256.Create())
+        {
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            return Convert.ToHexString(hashBytes); // .NET 5 +
+        }
+    }
+}
