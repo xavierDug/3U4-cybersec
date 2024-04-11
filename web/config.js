43bb0d18c6fa4b27030722b7abd045adf9a11bb0
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-04-10 22:38:53 -0400

    Upgrade et nouveau déploiement

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "Code - Nom du cours",
     description: "Site de référence du cours de x",
-    nomUrl: "depinfo-template"
+    nomUrl: "depinfo-gabarit"
 }
 
 module.exports = config;
