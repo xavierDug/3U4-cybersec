856162cd9b72f542935afbd3a0bcc9c9727483b2
Joris Deguet <joris.deguet@gmail.com>
2024-10-17 15:34:45 -0400

    Cleanup

--- stock/consoleApp/DonneesSecurite.cs
+++ stock/consoleApp/DonneesSecurite.cs
@@ -45,20 +45,5 @@ namespace consoleApp
                 return Convert.ToHexString(hashBytes); // .NET 5 +
             }
         }
-
-        //SHA3_256 
-
-        public static string CreateSHA256(string input)
-        {
-            // Use input string to calculate MD5 hash
-            using (System.Security.Cryptography.SHA256 md5 = System.Security.Cryptography.SHA256.Create())
-            {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-                return Convert.ToHexString(hashBytes); // .NET 5 +
-            }
-        }
-
-
     }
 }
